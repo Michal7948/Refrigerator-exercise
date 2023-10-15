62a828d1aa9c899f13c6e87f5e495a971edaee7b
Michal Maaravi <Michal7948@gmail.com>
2023-10-15 23:05:58 +0300

    Created C# project

---
+++ RefrigeratorExe/RefrigeratorExe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefrigeratorExe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
